333324bb3aecd68a91364031e3eff68a5f43c602
MaryArbs <mary.arbachauskas@gmail.com>
2020-07-27 17:01:26 -0400

    set up google map component

--- src/App.js
+++ src/App.js
@@ -6,3 +6,14 @@ import {
   Marker
 } from "react-google-maps";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const MyMapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 30.7333, lng: 76.7794 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: 30.7333, lng: 76.7794 }} />
+      )}
+    </GoogleMap>
+  ))
+);
+
