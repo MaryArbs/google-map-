b491e1c9fbc125f5693bf757d8cd0690f2bd7806
MaryArbs <mary.arbachauskas@gmail.com>
2020-07-28 15:35:34 -0400

    setup app component

--- src/App.js
+++ src/App.js
@@ -16,4 +16,33 @@ const MyMapComponent = withScriptjs(
     </GoogleMap>
   ))
 );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="text-center">
+          <h1>GOOGLE MAPS API with REACT JS</h1>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              MAPS
+              <div>
+                <MyMapComponent
+                  isMarkerShown
+                  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAN7mwyR_B3NDDLnuwkLds1RU9IUSBklc8&v=3.exp&libraries=geometry,drawing,places"
+                  loadingElement={<div style={{ height: `100%` }} />}
+                  containerElement={<div style={{ height: `400px` }} />}
+                  mapElement={<div style={{ height: `100%` }} />}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
 
